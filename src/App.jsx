35ce28c305220105bb1601bc3552de9678f8eec5
cthogiri <cthogiri@apple.com>
2023-06-09 13:34:35 +0530

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import { Header } from "@/Features/Header";
 import { Box } from "@chakra-ui/react";
 import { Landing } from "./Features/Landing";
 import { LatestProjects } from "./Features/LatestProjects";
+import { Footer } from "./Features/Footer";
 //import { ImageSlider } from "./components/imageSlider/ImageSlider";
 
 
 export function App(){
 
       return (
+        <>
         <Box p={20}>
           <Header />
           <Landing />
           <LatestProjects/>
         </Box>
+        <Footer/>
+        </>
+        
       );
 }
--- src/Features/Footer.jsx
+++ src/Features/Footer.jsx
@@ -1,6 +1,25 @@
+import { HStack, Icon, Link } from "@chakra-ui/react";
+import { BsGithub,BsFacebook,BsInstagram,BsLinkedin } from "react-icons/bs";
 export function Footer(){
     return (
-        <>
-        </>
+        <HStack 
+        justify={"center"}
+        h={130}
+        bg={"secondary"}
+        gap={5}
+        >
+        <Link href="https://github.com/" target="_blank" >
+        <Icon as={BsGithub} w={8} h={8} color={"white"} />
+        </Link>
+        <Link href="https://in.linkedin.com/" target="_blank">
+        <Icon as={BsLinkedin} w={8} h={8} color={"#0D73C6"} bg={"white"} borderRadius={5} />
+        </Link>
+        <Link href="https://www.facebook.com/" target="_blank">
+        <Icon as={BsFacebook} w={8} h={8} color={"white"} />
+        </Link>
+        <Link href="https://www.instagram.com/" target="_blank">
+        <Icon as={BsInstagram} w={8} h={8}  color={"#E1306C"}/>
+        </Link>
+        </HStack>
     );
 }
